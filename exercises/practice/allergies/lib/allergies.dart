4f1e23ffb2c78094777fa617ced2143aae829fbf
ssdd33 <beeblu.0@gmail.com>
2023-04-21 16:43:03 +0900

    allergies

--- exercises/practice/allergies/lib/allergies.dart
+++ exercises/practice/allergies/lib/allergies.dart
@@ -1,3 +1,49 @@
+import 'dart:math';
+
 class Allergies {
-  // Put your code here
+  List<String> substances = [
+    'eggs',
+    'peanuts',
+    'shellfish',
+    'strawberries',
+    'tomatoes',
+    'chocolate',
+    'pollen',
+    'cats'
+  ];
+
+  bool allergicTo(String allergen, int totalScore) {
+    List<num> scores = substances
+        .map<num>((allegen) => pow(2, substances.indexOf(allegen)))
+        .toList();
+    List<num> smallers = scores.where((score) => score <= totalScore).toList();
+
+    while (smallers.length != 0) {
+      int lastScore = smallers.removeLast().toInt();
+      int indexOfLastScore = smallers.length;
+      if (substances[indexOfLastScore] == allergen) {
+        return true;
+      }
+      totalScore -= lastScore;
+    }
+
+    return false;
+  }
+
+  List<String> list(int totalScore) {
+    List<String> list = [];
+    List<num> scores = substances
+        .map<num>((allegen) => pow(2, substances.indexOf(allegen)))
+        .toList();
+    List<num> smallers = scores.where((score) => score <= totalScore).toList();
+
+    while (smallers.length != 0) {
+      int lastScore = smallers.removeLast().toInt();
+      int indexOfLastScore = smallers.length;
+      list.add(substances[indexOfLastScore]);
+      totalScore -= lastScore;
+    }
+
+    return list;
+  }
 }
