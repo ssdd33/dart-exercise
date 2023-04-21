b5a7b3fe4f48249df6322c83681fccd38b7f65f1
ssdd33 <beeblu.0@gmail.com>
2023-04-21 21:01:08 +0900

    anagram

--- exercises/practice/anagram/lib/anagram.dart
+++ exercises/practice/anagram/lib/anagram.dart
@@ -1,3 +1,14 @@
 class Anagram {
-  // Put your code here
+  List<String> findAnagrams(String word, List<String> words) {
+    final List<String> list = [];
+    int getTotalUniPoint(String word) {
+      return word.runes.fold<int>(0, (p, e) => p + e);
+    }
+
+    int wordPoints = getTotalUniPoint(word);
+    words.forEach((w) => {
+          if (getTotalUniPoint(w) == wordPoints) {list.add(w)}
+        });
+    return list;
+  }
 }
