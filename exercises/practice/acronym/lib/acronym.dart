c96ebc1d5617ab6fa80eb4d52af20dba01578ee7
ssdd33 <beeblu.0@gmail.com>
2023-04-21 16:42:53 +0900

    acronym

--- exercises/practice/acronym/lib/acronym.dart
+++ exercises/practice/acronym/lib/acronym.dart
@@ -1,3 +1,7 @@
 class Acronym {
-  // Put your code here
+  String abreviate(String fullString) {
+    List<String> words = fullString.split(' ');
+    String acronym = words.reduce((value, element) => value + element[0]);
+    return acronym;
+  }
 }
