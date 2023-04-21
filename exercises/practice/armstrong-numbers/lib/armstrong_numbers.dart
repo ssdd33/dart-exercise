f7a4b286ca5b0bdf2b8dab28bb8056240353c1bc
ssdd33 <beeblu.0@gmail.com>
2023-04-21 22:05:04 +0900

    resolve amstrong_numbers

--- exercises/practice/armstrong-numbers/lib/armstrong_numbers.dart
+++ exercises/practice/armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,3 +1,13 @@
+import 'dart:math';
+
 class ArmstrongNumbers {
-  // Put your code here
+  bool isArmstrongNumber(String numString) {
+    int num = int.parse(numString);
+    int length = numString.length;
+    List<int> digits =
+        numString.split('').map((string) => int.parse(string)).toList();
+    int sum = digits.fold<int>(0, (p, e) => p + pow(e, length).toInt());
+
+    return sum == num;
+  }
 }
