7d008322abeb9c0823cd69ec3e602eee0719f74c
ssdd33 <beeblu.0@gmail.com>
2023-04-29 19:48:13 +0900

    resolve bob

--- exercises/practice/bob/lib/bob.dart
+++ exercises/practice/bob/lib/bob.dart
@@ -1,3 +1,19 @@
 class Bob {
-  // Put your code here
+  String response(String words) {
+    if (words.replaceAll(RegExp(r'\r|\t|\n'), '').trim() == '') {
+      return "Fine. Be that way!";
+    }
+    if (words == words.toUpperCase() && words != words.toLowerCase()) {
+      if (words.trim()[words.length - 1] == '?') {
+        return "Calm down, I know what I'm doing!";
+      } else {
+        return "Whoa, chill out!";
+      }
+    }
+    if (words.trim()[words.trim().length - 1] == '?') {
+      return "Sure.";
+    }
+
+    return "Whatever.";
+  }
 }
