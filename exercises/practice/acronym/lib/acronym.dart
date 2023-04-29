4594cc9562716200eb6d98e551275b00220e742f
ssdd33 <beeblu.0@gmail.com>
2023-04-29 20:24:25 +0900

    resolve dart

--- exercises/practice/acronym/lib/acronym.dart
+++ exercises/practice/acronym/lib/acronym.dart
@@ -1,5 +1,5 @@
 class Acronym {
-  String abreviate(String fullString) {
+  String abbreviate(String fullString) {
     List<String> words = fullString.split(' ');
     String acronym = words.reduce((value, element) => value + element[0]);
     return acronym;
--- exercises/practice/darts/lib/darts.dart
+++ exercises/practice/darts/lib/darts.dart
@@ -1,3 +1,20 @@
+import 'dart:math';
+
 class Darts {
-  // Put your code here
+  int score(double x, double y) {
+    double distance = sqrt(pow(x, 2) + pow(y, 2));
+
+    if (distance <= 10 && distance > 5) {
+      return 1;
+    }
+    if (distance <= 5 && distance > 1) {
+      return 5;
+    }
+
+    if (distance <= 1) {
+      return 10;
+    }
+
+    return 0;
+  }
 }
