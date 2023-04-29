ab91c3be10fef2c66f3414e0cf509eb7e0010c4b
ssdd33 <beeblu.0@gmail.com>
2023-04-29 20:01:35 +0900

    resolve collatz_conjecture

--- exercises/practice/collatz-conjecture/lib/collatz_conjecture.dart
+++ exercises/practice/collatz-conjecture/lib/collatz_conjecture.dart
@@ -1,3 +1,20 @@
 class CollatzConjecture {
-  // Put your code here
+  int steps(int num) {
+    int steps = 0;
+
+    if (num <= 0) {
+      throw (ArgumentError('Only positive integers are allowed'));
+    }
+    while (num != 1) {
+      steps++;
+      if (num % 2 == 0) {
+        num = num ~/ 2;
+        continue;
+      } else {
+        num = num * 3 + 1;
+      }
+    }
+
+    return steps;
+  }
 }
