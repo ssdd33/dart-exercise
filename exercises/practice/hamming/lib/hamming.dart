1c8435df52b9755157facd28fc5b9963d5b6d135
ssdd33 <beeblu.0@gmail.com>
2023-05-03 17:46:22 +0900

    resolve hamming

--- exercises/practice/hamming/lib/hamming.dart
+++ exercises/practice/hamming/lib/hamming.dart
@@ -1,3 +1,20 @@
 class Hamming {
   // Put your code here
+  int distance(String dna1, String dna2) {
+    if (dna1.length != dna2.length) {
+      throw ArgumentError('strands must be of equal length');
+    }
+
+    if (dna1.allMatches(dna2).length != 0) {
+      return 0;
+    }
+    int distance = 0;
+    dna1.split('').asMap().forEach((key, value) {
+      if (value != dna2[key]) {
+        distance++;
+      }
+    });
+
+    return distance;
+  }
 }
--- exercises/practice/hamming/test/hamming_test.dart
+++ exercises/practice/hamming/test/hamming_test.dart
@@ -4,7 +4,9 @@ import 'package:test/test.dart';
 void main() {
   final hamming = Hamming();
 
-  final mustEqual = predicate((Error e) => e is ArgumentError && e.message == 'strands must be of equal length',
+  final mustEqual = predicate(
+      (Error e) =>
+          e is ArgumentError && e.message == 'strands must be of equal length',
       'an ArgumentError with the message "strands must be of equal length"');
 
   group('Hamming', () {
@@ -16,37 +18,37 @@ void main() {
     test('single letter identical strands', () {
       final result = hamming.distance('A', 'A');
       expect(result, equals(0));
-    }, skip: true);
+    }, skip: false);
 
     test('single letter different strands', () {
       final result = hamming.distance('G', 'T');
       expect(result, equals(1));
-    }, skip: true);
+    }, skip: false);
 
     test('long identical strands', () {
       final result = hamming.distance('GGACTGAAATCTG', 'GGACTGAAATCTG');
       expect(result, equals(0));
-    }, skip: true);
+    }, skip: false);
 
     test('long different strands', () {
       final result = hamming.distance('GGACGGATTCTG', 'AGGACGGATTCT');
       expect(result, equals(9));
-    }, skip: true);
+    }, skip: false);
 
     test('disallow first strand longer', () {
       expect(() => hamming.distance('AATG', 'AAA'), throwsA(mustEqual));
-    }, skip: true);
+    }, skip: false);
 
     test('disallow second strand longer', () {
       expect(() => hamming.distance('ATA', 'AGTG'), throwsA(mustEqual));
-    }, skip: true);
+    }, skip: false);
 
     test('disallow empty first strand', () {
       expect(() => hamming.distance('', 'G'), throwsA(mustEqual));
-    }, skip: true);
+    }, skip: false);
 
     test('disallow empty second strand', () {
       expect(() => hamming.distance('G', ''), throwsA(mustEqual));
-    }, skip: true);
+    }, skip: false);
   });
 }
