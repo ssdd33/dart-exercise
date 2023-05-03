9b7e61fd81574616fafe704113c066313a9d3862
ssdd33 <beeblu.0@gmail.com>
2023-05-03 18:00:59 +0900

    resolve high_scores

--- exercises/practice/high-scores/lib/high_scores.dart
+++ exercises/practice/high-scores/lib/high_scores.dart
@@ -1,3 +1,22 @@
+import 'dart:math';
+
 class HighScores {
-  // Put your code here
+  List<int> scores;
+  HighScores(
+    this.scores,
+  );
+
+  int latest() {
+    return scores[scores.length - 1];
+  }
+
+  int personalBest() {
+    return scores.reduce((value, element) => max(value, element));
+  }
+
+  List<int> personalTopThree() {
+    List<int> copy = [...scores];
+    copy.sort((a, b) => -(a.compareTo(b)));
+    return copy.length < 3 ? copy : copy.sublist(0, 3);
+  }
 }
