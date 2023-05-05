abce327fdff7cecbe9a35546398bccbb07419a2a
ssdd33 <beeblu.0@gmail.com>
2023-05-05 09:56:54 +0900

    resolve isbn_verifier

--- exercises/practice/isbn-verifier/lib/isbn_verifier.dart
+++ exercises/practice/isbn-verifier/lib/isbn_verifier.dart
@@ -1,1 +1,39 @@
-// Put your code here
+bool isValid(String isbn) {
+  /*
+  1.check length
+  2.check if there is character in middle of isbn
+  3.if last isbn is character check if its 'X'
+  4.check its mod11 == 0
+  */
+
+  List<String> chars = isbn.replaceAll('-', '').split('');
+  if (chars.length != 10) {
+    return false;
+  }
+  int sum = 0;
+  for (var i = 0; i < 10; i++) {
+    if (!isNumeric(chars[i])) {
+      if (i < 9) {
+        return false;
+      } else if (chars[i] != 'X') {
+        return false;
+      }
+      sum += 10;
+    }
+    sum += int.parse(chars[i]) * (10 - i);
+  }
+
+  if (sum % 11 != 0) {
+    return false;
+  }
+  return true;
+}
+
+bool isNumeric(String str) {
+  try {
+    int.parse(str);
+    return true;
+  } on FormatException {
+    return false;
+  }
+}
