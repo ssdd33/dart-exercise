8832cbcdee151875d0568cfdb7ac124a881aafc8
ssdd33 <beeblu.0@gmail.com>
2023-05-11 16:17:12 +0900

    resolve matching_brackets

--- exercises/practice/matching-brackets/lib/matching_brackets.dart
+++ exercises/practice/matching-brackets/lib/matching_brackets.dart
@@ -1,3 +1,29 @@
 class MatchingBrackets {
-  // Put your code here
+  bool isPaired(String string) {
+    RegExp r = RegExp(r'\s|[^\(\)\{\}\[\]]');
+    String str = string.replaceAll(r, '');
+
+    Map<String, String> pairs = {"(": ")", "{": "}", "[": "]"};
+    final stack = <String>[];
+
+    for (int i = 0; i < str.length; i++) {
+      String? pairBracket = pairs[str[i]];
+
+      if (pairBracket != null) {
+        stack.add(pairBracket);
+      } else {
+        String stackTop = '';
+        try {
+          stackTop = stack.removeLast();
+          if (stackTop != str[i]) {
+            throw (Error);
+          }
+        } catch (e) {
+          return false;
+        }
+      }
+    }
+
+    return stack.length == 0;
+  }
 }
