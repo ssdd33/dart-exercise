f10284465c3904bd648821b5915dbd9bcbd0e5c5
ssdd33 <beeblu.0@gmail.com>
2023-05-12 17:55:05 +0900

    nth_prime...

--- exercises/practice/nth-prime/lib/nth_prime.dart
+++ exercises/practice/nth-prime/lib/nth_prime.dart
@@ -1,3 +1,13 @@
 class NthPrime {
-  // Put your code here
+  static final _primeList = <int>[];
+  int prime(int nth) {
+    if (nth <= _primeList.length) {
+      return _primeList[nth - 1];
+    } else {
+      int currentNum = 2
+      while (_primeList.length < nth) {
+
+      }
+    }
+  }
 }
