484ca7af153e6b6f699944ea3d3d47b8e046eb4a
yeseul-kim-bepluslab <suess0701@beplushealthcare.com>
2023-08-09 10:24:31 +0900

    pascal_triangle

--- exercises/practice/pascals-triangle/lib/pascals_triangle.dart
+++ exercises/practice/pascals-triangle/lib/pascals_triangle.dart
@@ -1,3 +1,39 @@
 class PascalsTriangle {
-  // Put your code here
+  List<List<int>> rows(int count) {
+/*                   0
+                <int>[1], 1
+              <int>[1, 1], 2
+            <int>[1, 2, 1], 3
+          <int>[1, 3, 3, 1], 4
+        <int>[1, 4, 6, 4, 1], 5
+      <int>[1, 5, 10, 10, 5, 1]
+
+triangle[3-1][1] = triangle[2-1][0] + triangle[2-1][1]
+triangle[4-1][1] = triangle[3-1][1-1] + triangle[3-1][1-0]
+triangle[4-1][2] = triangle[3-1][2-1] + triangle[3-1][2-0]
+triangle[5-1][1] = triangle[3-1][2-1] + triangle[3-1][2-0]
+
+
+*/
+
+    List<List<int>> result = [];
+
+    for (int rowIndex = 1; rowIndex <= count; rowIndex++) {
+      List<int> row = [];
+      for (int i = 1; i <= count; i++) {
+        //make row
+        if (i == 1 || i == count) {
+          row.add(1);
+        } else {
+          int number = result[rowIndex - 1][i - 1] + result[rowIndex - 1][i];
+          row.add(number);
+        }
+
+        //add row to triangle
+        result.add(row);
+      }
+    }
+
+    return result;
+  }
 }
