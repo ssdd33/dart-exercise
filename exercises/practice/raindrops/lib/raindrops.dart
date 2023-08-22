957afa40cd4ecd214c1a5c84cbf83928e96d7b22
ssdd33 <beeblu.0@gmail.com>
2023-08-22 09:07:07 +0900

    raindrop solved

--- exercises/practice/raindrops/lib/raindrops.dart
+++ exercises/practice/raindrops/lib/raindrops.dart
@@ -1,3 +1,23 @@
 class Raindrops {
-  // Put your code here
+  String convert(int value) {
+    String result = '';
+
+    if (value % 3 == 0) {
+      result += 'pling';
+    }
+
+    if (value % 5 == 0) {
+      result += 'plang';
+    }
+
+    if (value % 7 == 0) {
+      result += 'plong';
+    }
+
+    if (result == '') {
+      result = '$value';
+    }
+
+    return result;
+  }
 }
