7a4f6e0be2293cc7044286ea0474173dcee8b29c
ssdd33 <beeblu.0@gmail.com>
2023-08-24 09:41:29 +0900

    robot_simulator solved

--- exercises/practice/robot-simulator/lib/robot_simulator.dart
+++ exercises/practice/robot-simulator/lib/robot_simulator.dart
@@ -1,3 +1,101 @@
+import 'package:robot_simulator/orientation.dart';
+import 'package:robot_simulator/position.dart';
+
 class Robot {
-  // Put your code here
+  Position position;
+  Orientation orientation;
+  Robot(
+    this.position,
+    this.orientation,
+  );
+
+  void move(String directs) {
+/*
+linkedList direction [E,S,W,N]
+turn Left ->  idxOf currentDirection -1
+turn right - >                       +1
+
+advance rule
+E: x++
+S: y--
+W: x--
+N: y++
+*/
+
+    for (String direct in directs.split('')) {
+      switch (direct) {
+        case 'A':
+          {
+            _advance();
+          }
+          break;
+        default:
+          {
+            _turn(direct);
+          }
+          break;
+      }
+    }
+  }
+
+  void _advance() {
+    switch (orientation) {
+      case Orientation.north:
+        {
+          position = Position(position.x, position.y + 1);
+        }
+        break;
+      case Orientation.east:
+        {
+          position = Position(position.x + 1, position.y);
+        }
+        break;
+      case Orientation.south:
+        {
+          position = Position(position.x, position.y - 1);
+        }
+        break;
+      case Orientation.west:
+        {
+          position = Position(position.x - 1, position.y);
+        }
+        break;
+    }
+  }
+
+  void _turn(String side) {
+    print(side);
+    final orientations = Orientation.values.toList();
+    int index = orientations.indexOf(orientation);
+    print(index);
+
+    switch (side) {
+      case 'L':
+        {
+          orientation = _linkedListOfOrientation(index - 1);
+        }
+        break;
+      case 'R':
+        {
+          print(orientation);
+          orientation = _linkedListOfOrientation(index + 1);
+          print(orientation);
+        }
+        break;
+    }
+  }
+
+  Orientation _linkedListOfOrientation(int index) {
+    final orientations = Orientation.values.toList();
+
+    while (index > 3 || index < 0) {
+      if (index > 3) {
+        index -= 4;
+      } else if (index < 0) {
+        index += 4;
+      }
+    }
+
+    return orientations[index];
+  }
 }
--- exercises/practice/robot-simulator/test/robot_simulator_test.dart
+++ exercises/practice/robot-simulator/test/robot_simulator_test.dart
@@ -3,7 +3,8 @@ import 'package:robot_simulator/position.dart';
 import 'package:robot_simulator/robot_simulator.dart';
 import 'package:test/test.dart';
 
-void expected(Robot robot, {required Position beAt, required Orientation faceTo}) {
+void expected(Robot robot,
+    {required Position beAt, required Orientation faceTo}) {
   expect(robot.position, equals(beAt));
   expect(robot.orientation, equals(faceTo));
 }
@@ -11,9 +12,11 @@ void expected(Robot robot, {required Position beAt, required Orientation faceTo}
 void main() {
   group('RobotSimulator: Create robot - ', createRobot);
   group('RobotSimulator: Rotating clockwise - ', rotatingClockwise);
-  group('RobotSimulator: Rotating counter-clockwise - ', rotatingCounterClockwise);
+  group('RobotSimulator: Rotating counter-clockwise - ',
+      rotatingCounterClockwise);
   group('RobotSimulator: Moving forward one - ', movingForwardOne);
-  group('RobotSimulator: Follow series of instructions - ', followSeriesOfInstructions);
+  group('RobotSimulator: Follow series of instructions - ',
+      followSeriesOfInstructions);
 }
 
 void createRobot() {
@@ -25,7 +28,7 @@ void createRobot() {
   test('at negative position facing south', () {
     final robot = Robot(Position(-1, -1), Orientation.south);
     expected(robot, beAt: Position(-1, -1), faceTo: Orientation.south);
-  }, skip: true);
+  }, skip: false);
 }
 
 void rotatingClockwise() {
@@ -33,25 +36,25 @@ void rotatingClockwise() {
     final robot = Robot(Position(0, 0), Orientation.north);
     robot.move('R');
     expected(robot, beAt: Position(0, 0), faceTo: Orientation.east);
-  }, skip: true);
+  }, skip: false);
 
   test('changes east to south', () {
     final robot = Robot(Position(0, 0), Orientation.east);
     robot.move('R');
     expected(robot, beAt: Position(0, 0), faceTo: Orientation.south);
-  }, skip: true);
+  }, skip: false);
 
   test('changes south to west', () {
     final robot = Robot(Position(0, 0), Orientation.south);
     robot.move('R');
     expected(robot, beAt: Position(0, 0), faceTo: Orientation.west);
-  }, skip: true);
+  }, skip: false);
 
   test('changes west to north', () {
     final robot = Robot(Position(0, 0), Orientation.west);
     robot.move('R');
     expected(robot, beAt: Position(0, 0), faceTo: Orientation.north);
-  }, skip: true);
+  }, skip: false);
 }
 
 void rotatingCounterClockwise() {
@@ -59,25 +62,25 @@ void rotatingCounterClockwise() {
     final robot = Robot(Position(0, 0), Orientation.north);
     robot.move('L');
     expected(robot, beAt: Position(0, 0), faceTo: Orientation.west);
-  }, skip: true);
+  }, skip: false);
 
   test('changes west to south', () {
     final robot = Robot(Position(0, 0), Orientation.west);
     robot.move('L');
     expected(robot, beAt: Position(0, 0), faceTo: Orientation.south);
-  }, skip: true);
+  }, skip: false);
 
   test('changes south to east', () {
     final robot = Robot(Position(0, 0), Orientation.south);
     robot.move('L');
     expected(robot, beAt: Position(0, 0), faceTo: Orientation.east);
-  }, skip: true);
+  }, skip: false);
 
   test('changes east to north', () {
     final robot = Robot(Position(0, 0), Orientation.east);
     robot.move('L');
     expected(robot, beAt: Position(0, 0), faceTo: Orientation.north);
-  }, skip: true);
+  }, skip: false);
 }
 
 void movingForwardOne() {
@@ -85,25 +88,25 @@ void movingForwardOne() {
     final robot = Robot(Position(0, 0), Orientation.north);
     robot.move('A');
     expected(robot, beAt: Position(0, 1), faceTo: Orientation.north);
-  }, skip: true);
+  }, skip: false);
 
   test('facing south decrements Y', () {
     final robot = Robot(Position(0, 0), Orientation.south);
     robot.move('A');
     expected(robot, beAt: Position(0, -1), faceTo: Orientation.south);
-  }, skip: true);
+  }, skip: false);
 
   test('facing east increments X', () {
     final robot = Robot(Position(0, 0), Orientation.east);
     robot.move('A');
     expected(robot, beAt: Position(1, 0), faceTo: Orientation.east);
-  }, skip: true);
+  }, skip: false);
 
   test('facing west decrements X', () {
     final robot = Robot(Position(0, 0), Orientation.west);
     robot.move('A');
     expected(robot, beAt: Position(-1, 0), faceTo: Orientation.west);
-  }, skip: true);
+  }, skip: false);
 }
 
 void followSeriesOfInstructions() {
@@ -111,23 +114,23 @@ void followSeriesOfInstructions() {
     final robot = Robot(Position(7, 3), Orientation.north);
     robot.move('RAALAL');
     expected(robot, beAt: Position(9, 4), faceTo: Orientation.west);
-  }, skip: true);
+  }, skip: false);
 
   test('moving west and north', () {
     final robot = Robot(Position(0, 0), Orientation.north);
     robot.move('LAAARALA');
     expected(robot, beAt: Position(-4, 1), faceTo: Orientation.west);
-  }, skip: true);
+  }, skip: false);
 
   test('moving west and south', () {
     final robot = Robot(Position(2, -7), Orientation.east);
     robot.move('RRAAAAALA');
     expected(robot, beAt: Position(-3, -8), faceTo: Orientation.south);
-  }, skip: true);
+  }, skip: false);
 
   test('moving east and north', () {
     final robot = Robot(Position(8, 4), Orientation.south);
     robot.move('LAAARRRALLLL');
     expected(robot, beAt: Position(11, 5), faceTo: Orientation.north);
-  }, skip: true);
+  }, skip: false);
 }
