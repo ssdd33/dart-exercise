39d0f211aa21bfd8429160db2576ade0bb0b3c8b
ssdd33 <beeblu.0@gmail.com>
2023-09-11 11:12:11 +0900

    anagram solved

--- exercises/practice/anagram/lib/anagram.dart
+++ exercises/practice/anagram/lib/anagram.dart
@@ -1,13 +1,25 @@
 class Anagram {
   List<String> findAnagrams(String word, List<String> words) {
     final List<String> list = [];
-    int getTotalUniPoint(String word) {
-      return word.runes.fold<int>(0, (p, e) => p + e);
+
+    bool _isAnagram(String word2) {
+      if (word.length != word2.length ||
+          word.toLowerCase() == word2.toLowerCase()) return false;
+
+      List<String> chars1 = word.toLowerCase().split('');
+      word2 = word2.toLowerCase();
+
+      for (int i = 0; i < word2.length; i++) {
+        int indexOfChar = chars1.indexOf(word2[i]);
+        if (indexOfChar < 0) return false;
+        chars1.removeAt(indexOfChar);
+      }
+
+      return true;
     }
 
-    int wordPoints = getTotalUniPoint(word);
     words.forEach((w) => {
-          if (getTotalUniPoint(w) == wordPoints) {list.add(w)}
+          if (_isAnagram(w)) {list.add(w)}
         });
     return list;
   }
