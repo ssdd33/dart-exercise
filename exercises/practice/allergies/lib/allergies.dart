03dda7eaeb00fb60d37e94093160ae75990c8f4a
ssdd33 <beeblu.0@gmail.com>
2023-09-20 10:58:17 +0900

    scret_handshake solved

--- exercises/practice/allergies/lib/allergies.dart
+++ exercises/practice/allergies/lib/allergies.dart
@@ -1,7 +1,55 @@
-import 'dart:math';
+// import 'dart:math';
+
+// class Allergies {
+//   List<String> substances = [
+//     'eggs',
+//     'peanuts',
+//     'shellfish',
+//     'strawberries',
+//     'tomatoes',
+//     'chocolate',
+//     'pollen',
+//     'cats'
+//   ];
+
+//   bool allergicTo(String allergen, int totalScore) {
+//     List<num> scores = substances
+//         .map<num>((allegen) => pow(2, substances.indexOf(allegen)))
+//         .toList();
+//     List<num> smallers = scores.where((score) => score <= totalScore).toList();
+
+//     while (smallers.length != 0) {
+//       int lastScore = smallers.removeLast().toInt();
+//       int indexOfLastScore = smallers.length;
+//       if (substances[indexOfLastScore] == allergen) {
+//         return true;
+//       }
+//       totalScore -= lastScore;
+//     }
+
+//     return false;
+//   }
+
+//   List<String> list(int totalScore) {
+//     List<String> list = [];
+//     List<num> scores = substances
+//         .map<num>((allegen) => pow(2, substances.indexOf(allegen)))
+//         .toList();
+//     List<num> smallers = scores.where((score) => score <= totalScore).toList();
+
+//     while (smallers.length != 0) {
+//       int lastScore = smallers.removeLast().toInt();
+//       int indexOfLastScore = smallers.length;
+//       list.add(substances[indexOfLastScore]);
+//       totalScore -= lastScore;
+//     }
+
+//     return list;
+//   }
+// }
 
 class Allergies {
-  List<String> substances = [
+  final substances = [
     'eggs',
     'peanuts',
     'shellfish',
@@ -12,38 +60,13 @@ class Allergies {
     'cats'
   ];
 
-  bool allergicTo(String allergen, int totalScore) {
-    List<num> scores = substances
-        .map<num>((allegen) => pow(2, substances.indexOf(allegen)))
-        .toList();
-    List<num> smallers = scores.where((score) => score <= totalScore).toList();
-
-    while (smallers.length != 0) {
-      int lastScore = smallers.removeLast().toInt();
-      int indexOfLastScore = smallers.length;
-      if (substances[indexOfLastScore] == allergen) {
-        return true;
-      }
-      totalScore -= lastScore;
-    }
-
-    return false;
+  bool allergicTo(String item, int score) {
+    int index = substances.indexOf(item);
+    print(
+        's:$score, i:$index, s>>i:${score >> index}  s>>i&1:${(score >> index) & 1}, s>>1:${score >> 1} ');
+    return ((score >> index) & 1) == 1;
   }
 
-  List<String> list(int totalScore) {
-    List<String> list = [];
-    List<num> scores = substances
-        .map<num>((allegen) => pow(2, substances.indexOf(allegen)))
-        .toList();
-    List<num> smallers = scores.where((score) => score <= totalScore).toList();
-
-    while (smallers.length != 0) {
-      int lastScore = smallers.removeLast().toInt();
-      int indexOfLastScore = smallers.length;
-      list.add(substances[indexOfLastScore]);
-      totalScore -= lastScore;
-    }
-
-    return list;
-  }
+  List<String> list(int score) =>
+      substances.where((t) => allergicTo(t, score)).toList();
 }
--- exercises/practice/secret-handshake/lib/secret_handshake.dart
+++ exercises/practice/secret-handshake/lib/secret_handshake.dart
@@ -1,3 +1,24 @@
 class SecretHandshake {
-  // Put your code here
+  List<String> actions = ['wink', 'double blink', 'close your eyes', 'jump'];
+  List<String> commands(int num) {
+    List<String> result = <String>[];
+    int index = 0;
+    int currentNum = num;
+    if (currentNum > 0) {
+      while (currentNum > 1) {
+        if (currentNum % 2 == 1) {
+          result.add(actions[index]);
+        }
+        currentNum = (currentNum / 2).floor();
+        index++;
+      }
+      if (index < 4) {
+        result.add(actions[index]);
+      } else {
+        result = result.reversed.toList();
+      }
+    }
+
+    return result;
+  }
 }
