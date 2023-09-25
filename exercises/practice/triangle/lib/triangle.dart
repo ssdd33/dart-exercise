c22cb0d6121426a1f0745ed9504af33f20c69ea7
ssdd33 <beeblu.0@gmail.com>
2023-09-25 10:56:16 +0900

    trianlge solved

--- exercises/practice/triangle/lib/triangle.dart
+++ exercises/practice/triangle/lib/triangle.dart
@@ -1,3 +1,26 @@
 class Triangle {
-  // Put your code here
+  bool equilateral(double a, double b, double c) {
+    if (_isTriangle(a, b, c)) {
+      return a == b && b == c;
+    }
+    return false;
+  }
+
+  bool isosceles(double a, double b, double c) {
+    if (_isTriangle(a, b, c)) {
+      return a == b || b == c || a == c;
+    }
+    return false;
+  }
+
+  bool scalene(double a, double b, double c) {
+    if (_isTriangle(a, b, c)) {
+      return !equilateral(a, b, c) && !isosceles(a, b, c);
+    }
+    return false;
+  }
+
+  bool _isTriangle(double a, double b, double c) {
+    return a != 0 && b != 0 && c != 0 && a + b >= c && a + c >= b && b + c >= a;
+  }
 }
